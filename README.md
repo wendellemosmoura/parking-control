565cbbcee1233049e734cbeb5cf03354401647f2
wendel.lemos <wendel_lemos_cg@hotmail.com>
2022-11-21 18:03:36 -0300

    Autenticação em base de dados via JPA.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# parking-control
-Projeto "Parking Control" do curso Spring Boot | Curso Completo 2022 da professora Michelli Brito onde explora tanto a parte teórica quanto prática do Spring, 
-desde a base do Spring Framework, Inversão de Controle, Injeção de Dependências, Beans, definição Spring Boot e também a construção de um projeto Spring Boot 
-(Parking Control API) do zero, com projetos como Spring MVC, Spring Data JPA e Spring Validation para a criação de uma API com Pageable, 
-métodos getAll, getOne, post, delete, update e Global Custom Date.
+# Parking Control API
+Projeto "Parking Control API" do curso "Spring Boot | Curso Completo 2022" da professora Michelli Brito onde explora tanto a parte teórica quanto prática do Spring, 
+desde a base do Spring Framework, inversão de controle, injeção de dependências, Beans, definições do Spring Boot e também a construção da API do zero com banco de dados PostgreSQL,
+métodos getAll pageable, getOne, post, delete, update e Global Custom Date, usando os projetos Spring MVC, Spring Data JPA, Spring Validation e Spring Security.
---
+++ src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -0,0 +1,62 @@
+package com.api.parkingcontrol.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TB_USER")
+public class UserModel implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false,unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
